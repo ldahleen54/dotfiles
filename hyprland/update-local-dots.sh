6676b590cd09cc8ff1c6c25e6c69b29881cc4d4b
Luke Dahleen <ldahleen54@gmail.com>
2025-11-12 22:43:53 -0600

    Update config files and update the shell scripts

--- hyprland/update-local-dots.sh
+++ hyprland/update-local-dots.sh
@@ -2,9 +2,10 @@
 # Updates the config files locally from this repo
 
 # set the variables
-export REPO_PATH="/run/media/luke/Storage/git/dotfiles"
 export CONFIG_PATH="/home/luke/.config/hypr"
 
 # copy the config files
-cp -r ./UserConfigs ~/.config/hypr/
-cp monitors.conf ~/.config/hypr
+cp -r ./UserConfigs $CONFIG_PATH
+cp monitors.conf $CONFIG_PATH
+cp workspaces.conf "$CONFIG_PATH" 
+cp -r ./configs $CONFIG_PATH
--- hyprland/update-repo.sh
+++ hyprland/update-repo.sh
@@ -2,14 +2,13 @@
 # Updates the repo by pulling from local configs
 
 # set the variables
-export REPO_PATH="/run/media/luke/Storage/git/dotfiles"
 export CONFIG_PATH="/home/luke/.config/hypr"
 
 # copy the config files to this repo to commit
-cp -r $CONFIG_PATH/UserConfigs $REPO_PATH/hyprland/
-cp -r $CONFIG_PATH/monitors.conf $REPO_PATH/hyprland/
-echo "$CONFIG_PATH/monitors.conf"
-cp "$CONFIG_PATH/workspaces.conf" $REPO_PATH/hyprland/
+cp -r $CONFIG_PATH/UserConfigs ./
+cp $CONFIG_PATH/monitors.conf ./
+cp "$CONFIG_PATH/workspaces.conf" ./
+cp -r "$CONFIG_PATH/configs" ./
 
 # git add hyprland/
 # git commit -m "update dot files"
