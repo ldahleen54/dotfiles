76d9c71c690c66690d0d640a22fd7641280231cb
Luke Dahleen <ldahleen54@gmail.com>
2026-01-17 13:44:05 -0600

    Remove drawers for waybar

--- hyprland/update-local-dots.sh
+++ hyprland/update-local-dots.sh
@@ -3,9 +3,11 @@
 
 # set the variables
 export CONFIG_PATH="/home/luke/.config/hypr"
+export WAYBAR_PATH="/home/luke/.config/waybar/ModulesGroups"
 
 # copy the config files
 cp -r ./UserConfigs $CONFIG_PATH
 cp monitors.conf $CONFIG_PATH
 cp workspaces.conf "$CONFIG_PATH" 
 cp -r ./configs $CONFIG_PATH
+cp ./waybar/ModulesGroups $WAYBAR_PATH
