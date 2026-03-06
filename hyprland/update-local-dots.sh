6f2f47aea50d4e95a635fd91b83533d47624cd8c
Luke Dahleen <ldahleen54@gmail.com>
2026-03-05 22:09:27 -0600

    update-local dots fix

--- hyprland/update-local-dots.sh
+++ hyprland/update-local-dots.sh
@@ -3,7 +3,7 @@
 
 # set the variables
 export CONFIG_PATH="/home/luke/.config/hypr"
-export WAYBAR_PATH="/home/luke/.config/waybar/ModulesGroups"
+export WAYBAR_PATH="/home/luke/.config/waybar"
 
 # copy the config files
 cp -r ./UserConfigs $CONFIG_PATH
@@ -12,6 +12,6 @@ cp workspaces.conf "$CONFIG_PATH"
 cp -r ./configs $CONFIG_PATH
 cp ./waybar/ModulesGroups $WAYBAR_PATH
 cp ./waybar/ModulesCustom $WAYBAR_PATH
-cp "./waybar/style/[TOP] Default" $WAYBAR_PATH
+cp "./waybar/style/[TOP] Default" $WAYBAR_PATH/style
 cp "./waybar/style.css" $WAYBAR_PATH
-cp "./waybar/scripts/color-clock.sh" $WAYBAR_PATH
+cp "./waybar/scripts/color-clock.sh" $WAYBAR_PATH/scripts
